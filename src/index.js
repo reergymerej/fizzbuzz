a80f0aa240f5d4a7a80767a8ad1b6394cdd8bfa6
Jeremy Greer <jex.grizzle@gmail.com>
2020-01-30 10:52:08 -0800

    Add phone challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-export default () => {
+export const fizzbuzz = () => {
+  throw new Error('not implemented')
+}
+
+export const phone = () => {
   throw new Error('not implemented')
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,6 @@
-import fizzbuzz from '.'
+import { fizzbuzz, phone } from '.'
 
-describe('complete results', () => {
+describe('fizzbuzz', () => {
   it('should return complete list as expected', () => {
     const result = fizzbuzz()
     const expected = [
@@ -107,4 +107,16 @@ describe('complete results', () => {
     ]
     expect(result).toEqual(expected)
   })
+})
+
+
+describe('phone', () => {
+  it.each`
+  input | expected
+  ${[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]}| ${'(123) 456-7890'}
+  ${[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}| ${'(111) 111-1111'}
+  `('returns $expected for $input', ({ input, expected }) => {
+  const result = phone(input)
+  expect(result).toEqual(expected)
+})
 })
